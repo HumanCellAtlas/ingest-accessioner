6fe32cde02841f5e6a8e278ce1fe6cbf1e5b17b8
Tony Burdett <tburdett@ebi.ac.uk>
2017-09-28 14:54:38 +0100

    logging and code cleanup

--- accessionprocessor.py
+++ accessionprocessor.py
@@ -1,4 +1,4 @@
-import logging, uuid, json, time
+import logging, uuid, json
 
 
 class AccessionProcessor:
@@ -22,8 +22,6 @@ class AccessionProcessor:
 
             entity_link = params['callbackLink']
             
-            time.sleep(1)
-
             if self.ingest_api.update_entity_if_match(entity_link, json.dumps(metadata_update)):
                 self.logger.info('updated entity accession uuid!')
             else:
